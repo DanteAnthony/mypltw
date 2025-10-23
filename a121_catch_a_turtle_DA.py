cfec9aee4a8169d146cfe60565b32a91cc29c244
DanteAnthony <dg.anthony0604@gmail.com>
2025-10-23 14:21:15 -0700

    a122 leaderboard

--- a121_catch_a_turtle_DA.py
+++ a121_catch_a_turtle_DA.py
@@ -1,6 +1,7 @@
 #-----import statements-----
 import turtle
 import random
+import leaderboard
 
 #-----game configuration----
 spot_color = "pink"
@@ -10,9 +11,11 @@ colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 sizes = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
 score = 0
 font_setup = ("Arial", 20, "normal")
-timer = 30
+timer = 5
 counter_interval = 1000 #1000 represents 1 second
 timer_up = False
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name = input("Please enter your name: ")
 
 #-----initialize turtle-----
 spot = turtle.Turtle()
@@ -64,15 +67,16 @@ def update_score():
     score_writer.write(score, font=font_setup)
 
 def countdown():
-  global timer, timer_up
-  counter.clear()
-  if timer <= 0:
-    counter.write("Time's Up", font=font_setup)
-    timer_up = True
-  else:
-    counter.write("Timer: " + str(timer), font=font_setup)
-    timer -= 1
-    counter.getscreen().ontimer(countdown, counter_interval) 
+   global timer, timer_up
+   counter.clear()
+   if timer <= 0:
+      counter.write("Time's Up", font=font_setup)
+      timer_up = True
+      manage_leaderboard()
+   else:
+      counter.write("Timer: " + str(timer), font=font_setup)
+      timer -= 1
+      counter.getscreen().ontimer(countdown, counter_interval) 
 
 def change_color():
    spot.fillcolor(random.choice(colors))
@@ -81,6 +85,22 @@ def change_color():
 
 def change_size():
    spot.shapesize(random.choice(sizes))
+
+def manage_leaderboard():
+  global score
+  global spot
+
+  # get the names and scores from the leaderboard file
+  leader_names_list = leaderboard.get_names(leaderboard_file_name)
+  leader_scores_list = leaderboard.get_scores(leaderboard_file_name)
+
+  # show the leaderboard with or without the current player
+  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    leaderboard.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    leaderboard.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
+
+  else:
+    leaderboard.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
 
 #-----events----------------
 spot.onclick(spot_clicked)
